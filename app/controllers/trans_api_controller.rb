806c7445016ed756149ec87e53374428967c0d7f
soil <sh.yoshida@live.jp>
2018-11-16 01:08:46 +0900

    update app-base

--- app/controllers/trans_api_controller.rb
+++ app/controllers/trans_api_controller.rb
@@ -1,4 +1,33 @@
+require 'yaml'
+
 class TransApiController < ApplicationController
   def index
+    puts params['text']
+
+    if params['text'] != nil then
+      text = params['text']
+    else
+      text = '良い旅を！'
+    end
+
+    if ENV['CONSUMER_KEY'].nil?
+      setting = YAML.load_file("app/secret/setting/apikey.yml")
+
+      consumer_key = setting['CONSUMER_KEY']
+      consumer_secret = setting['CONSUMER_SECRET']
+      url = setting['URL']
+      name = setting['NAME']
+    else
+      consumer_key = ENV['CONSUMER_KEY']
+      consumer_secret = ENV['CONSUMER_SECRET']
+      url = ENV['URL']
+      name = ENV['NAME']
+    end
+
+    consumer = OAuth::Consumer.new(consumer_key, consumer_secret)
+    endpoint = OAuth::AccessToken.new(consumer)
+
+    response = endpoint.post(url,{key: consumer_key,type: 'json',name: name, text: text})
+    render json: JSON.parse(response.body)
   end
 end
